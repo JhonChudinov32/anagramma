5c77f33cb754e849fafd4d9e9501e44fe897bc3f
WIN-2SDGFMI4Q5P\User <chudinov1989em@mail.ru>
2020-11-18 13:36:30 +0300

    Добавьте файлы проекта.

---
+++ Anagramma/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Anagramma
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите текст: ");
+            var text = Console.ReadLine();
+            var split = text.Split();
+            var s = "";
+            foreach (var item in split)
+            {
+                s += Reverse(item.ToList()) + " ";
+            }
+            Console.WriteLine($"{text} => {s}");
+            Console.ReadLine();
+        }
+
+        private static string Reverse(List<char> word) 
+        {
+            var list = new List<(int ind, char ch)>();
+            for (int i = 0; i < word.Count; i++)
+            {
+                if (!char.IsLetter(word[i]))
+                {
+                    list.Add((i, word[i]));
+                }
+            }
+            foreach (var item in word.ToList())
+            {
+                if (!char.IsLetter(item))
+                {
+                    word.Remove(item);
+                }
+            }
+            word.Reverse();
+            foreach (var (ind, ch) in list)
+            {
+                word.Insert(ind, ch);
+            }
+            return string.Join("", word);
+        }
+
+
+    }
+}
